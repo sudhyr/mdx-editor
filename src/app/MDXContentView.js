65e5fa6a8d2ab1e9e20e09fdf49b565c13ec2a65
Rihdus <rihdus.io@gmail.com>
2021-08-01 01:34:33 +0800

    Load content from file

--- src/app/MDXContentView.js
+++ src/app/MDXContentView.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import s from './common.css';
+import React, { useState } from "react";
+import s from "./common.css";
 
 export default function PageEditor() {
+  const [data, setData] = useState("");
+
+  const onOpenFileClick = async (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+    const reader = new FileReader();
+    reader.onload = () => {
+      setData(reader.result);
+    };
+    reader.readAsText(file);
+  };
+
   return (
     <div className={s.appBody}>
       <div className={s.explorerView}>
-        <pre>MDX content goes here</pre>
-        <h1></h1>
+        <input type="file" onChange={onOpenFileClick} />
+        <pre>{data}</pre>
       </div>
     </div>
-  )
+  );
 }
