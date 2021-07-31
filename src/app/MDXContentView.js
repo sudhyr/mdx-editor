0a9d0643fa6cad100002ad1feb3824c758662918
Rihdus <rihdus.io@gmail.com>
2021-08-01 01:57:20 +0800

    read and write to file

--- src/app/MDXContentView.js
+++ src/app/MDXContentView.js
@@ -1,24 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import s from "./common.css";
 
 export default function PageEditor() {
+  const fileHandle = useRef(null);
   const [data, setData] = useState("");
+  const [updatedData, setUpdatedData] = useState("");
 
-  const onOpenFileClick = async (e) => {
+  const onChangeFileClick = async (e) => {
     const file = e.target.files[0];
-    console.log(file);
+    fileHandle.current = file;
     const reader = new FileReader();
     reader.onload = () => {
       setData(reader.result);
+      setUpdatedData(reader.result);
     };
     reader.readAsText(file);
+  };
+
+  const onOpenFile = async () => {
+    const [newFsHandle] = await window.showOpenFilePicker({
+      types: [
+        {
+          description: "Markdown Files",
+        },
+      ],
+      excludeAcceptAllOption: true,
+      multiple: false,
+    });
+
+    fileHandle.current = newFsHandle;
+    const reader = new FileReader();
+    reader.onload = () => {
+      setData(reader.result);
+      setUpdatedData(reader.result);
+    };
+    const file = await newFsHandle.getFile();
+    reader.readAsText(file);
+  };
+
+  const onSaveClick = async () => {
+    const writable = await fileHandle.current.createWritable();
+
+    // Write the contents of the file to the stream.
+    await writable.write(updatedData);
+
+    // Close the file and write the contents to disk.
+    await writable.close();
   };
 
   return (
     <div className={s.appBody}>
       <div className={s.explorerView}>
-        <input type="file" onChange={onOpenFileClick} />
-        <pre>{data}</pre>
+        <div>
+          <button onClick={onOpenFile}>Open File</button>
+          {/* <input type="file" onChange={onOpenFileClick} /> */}
+
+          <textarea
+            defaultValue={data}
+            onChange={(e) => setUpdatedData(e.target.value)}
+          />
+
+          <button onClick={onSaveClick}>Save</button>
+        </div>
       </div>
     </div>
   );
