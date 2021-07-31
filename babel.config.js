611baa1abd93152078c5469c3502bf9bed9bc358
Rihdus <rihdus.io@gmail.com>
2021-08-01 01:34:14 +0800

    Add async support

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    [
+      '@babel/preset-env',
+      {
+        "useBuiltIns": "usage",
+        "corejs": "3"
+      }
+    ],
     '@babel/react',
     '@babel/typescript'
   ],
